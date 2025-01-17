027d9060e0732d0dd7d5d61cf4cb6b8b5682466c
khanlinuxops <mudassir.farooqcybersec@gmail.com>
2025-01-17 15:19:20 +0500

    i added a new game

---
+++ python_game.py
@@ -0,0 +1,28 @@
+import random
+
+def play_game():
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+    print("Can you guess it?")
+
+    number_to_guess = random.randint(1, 100)
+    attempts = 0
+
+    while True:
+        try:
+            guess = int(input("Enter your guess: "))
+            attempts += 1
+
+            if guess < number_to_guess:
+                print("Too low! Try again.")
+            elif guess > number_to_guess:
+                print("Too high! Try again.")
+            else:
+                print(f"Congratulations! You guessed the number in {attempts} attempts.")
+                break
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+if __name__ == "__main__":
+    play_game()
+
